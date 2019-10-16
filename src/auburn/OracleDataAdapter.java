dc836c358aa5d94f9689490c92bd2ade1e8d1bcd
cls-kokoro <cls0095@auburn.edu>
2019-10-16 01:05:59 -0500

    Initial commit

---
+++ src/auburn/OracleDataAdapter.java
@@ -0,0 +1,30 @@
+package auburn;
+
+public class OracleDataAdapter implements IDataAdapter {
+    public int connect(String dbfile) {
+        //...
+        return CONNECTION_OPEN_OK;
+    }
+
+    public int disconnect() {
+        // ...
+        return CONNECTION_CLOSE_OK;
+
+    }
+    public CustomerModel loadCustomer(int id) {
+        return null;
+    }
+
+    public ProductModel loadProduct(int id) {
+        return null;
+    }
+    public int saveProduct(ProductModel model) {
+        return PRODUCT_SAVED_OK;
+    }
+    public int saveCustomer(CustomerModel customer) {return CUSTOMER_SAVED_OK;}
+    @Override
+    public int savePurchase(PurchaseModel model) {
+        return 0;
+    }
+
+}
